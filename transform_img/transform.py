adf31c0c654338bf461d3053ae619a608209760d
d grossman <dgrossman@iqt.org>
2016-12-02 21:11:34 +0000

    multiprocessing of the images..

--- transform_img/transform.py
+++ transform_img/transform.py
@@ -22,9 +22,102 @@ import docopt
 import os
 import sys
 import time
+import itertools
+import functools
+import json
 
+from itertools import islice
 from PIL import Image, ImageFilter
 from resizeimage import resizeimage
+from multiprocessing import Pool
+from functools import partial
+
+def realworker(mfile_path,moutput_path, mis_blur, mis_grayscale, mis_ste,mste_percentage,
+        b_dir,s_dir,g_dir):
+# -----------------------------------------------------------------------------
+#  Transformation functions
+# -----------------------------------------------------------------------------
+
+    # define the functions that will transform the image
+    class TransformedImage(object):
+        def __init__(self, img, output_path,percentage,blur_dir,ste_dir,grayscale_dir):
+            self.img = img
+            self.output_path = output_path
+            self.percentage = percentage
+            self.blur_dir = blur_dir
+            self.ste_dir = ste_dir
+            self.grayscale_dir = grayscale_dir
+
+        def blur(self):
+            blur_img = self.img.filter(ImageFilter.BLUR)
+            blur_img_path = makepath(self.blur_dir, os.path.basename(self.img.filename))
+            blur_img.save(blur_img_path)
+            return
+
+        def shrink_then_enlarge(self, p=25):
+            width, height = self.img.size
+            resized_width = int((p/100.0) * width)
+            resized_height = int((p/100.0) * height)
+
+            # shrink by 50% with degradation
+            shrink_img = self.img.thumbnail((resized_width, resized_height))
+            # without degradation: self.img.thumbnail((5,5), Image.ANTIALIAS)
+            
+            # enlarge to original size
+            ste_img = self.img.resize((width , height))
+            ste_img_path = makepath(self.ste_dir, os.path.basename(self.img.filename))
+            ste_img.save(ste_img_path)
+            # without degradation: ste_img.save(ste_img_path, quality=90)
+            return
+
+        def grayscale(self):
+            grayscale_img = self.img.convert("1")
+            grayscale_img_path = makepath(self.grayscale_dir, os.path.basename(self.img.filename))
+            grayscale_img.save(grayscale_img_path)
+            return 
+
+    retval = dict()
+    img = TransformedImage(Image.open(mfile_path),output_path=moutput_path,percentage=mste_percentage,blur_dir=b_dir,ste_dir=s_dir,grayscale_dir=g_dir)
+    retval['img'] = img.img.filename
+    retval['blur']='none'
+    retval['grayscale']='none'
+    retval['ste']='none'
+    if mis_blur: 
+        retval['blur'] = timeit(img.blur)
+    if mis_grayscale: 
+        retval['grayscale'] = timeit(img.grayscale)
+    if mis_ste: 
+        retval['ste'] = timeit(img.shrink_then_enlarge,mste_percentage)
+
+    return json.dumps(retval)
+
+    
+
+def timeit(func, *args, **kwargs):
+    """ This is a wrapper function to calculate how fast each operation takes.
+    Note that we are assuming that we do not need to do anything with the return
+    value of the function.
+    Args: 
+        func: function pointer
+        args: arguments to the function
+        kwargs: named arguments not defined in advance to be passed in to the function
+    """
+    start = time.time()
+    func(*args, **kwargs)
+    elapsed = time.time() - start
+    return elapsed
+
+
+
+def grouper(n, iterable):
+    it = iter(iterable)
+    while True:
+       chunk = tuple(itertools.islice(it, n))
+       if not chunk:
+           return
+       yield chunk
+
+
 
 def main(args={}):
     """ Transform images in a directory
@@ -79,74 +172,31 @@ def main(args={}):
         makedir(grayscale_dir)
 
 # -----------------------------------------------------------------------------
-#  Transformation functions
-# -----------------------------------------------------------------------------
-
-    # define the functions that will transform the image
-    class TransformedImage(object):
-        def __init__(self, img, output_path):
-            self.img = img
-
-        def blur(self):
-            blur_img = self.img.filter(ImageFilter.BLUR)
-            blur_img_path = makepath(blur_dir, os.path.basename(self.img.filename))
-            blur_img.save(blur_img_path)
-            return
-
-        def shrink_then_enlarge(self, percentage=25):
-            width, height = self.img.size
-            resized_width = int((percentage/100.0) * width)
-            resized_height = int((percentage/100.0) * height)
-
-            # shrink by 50% with degradation
-            shrink_img = self.img.thumbnail((resized_width, resized_height))
-            # without degradation: self.img.thumbnail((5,5), Image.ANTIALIAS)
-            
-            # enlarge to original size
-            ste_img = self.img.resize((width , height))
-            ste_img_path = makepath(ste_dir, os.path.basename(self.img.filename))
-            ste_img.save(ste_img_path)
-            # without degradation: ste_img.save(ste_img_path, quality=90)
-            return
-
-        def grayscale(self):
-            grayscale_img = self.img.convert("1")
-            grayscale_img_path = makepath(grayscale_dir, os.path.basename(self.img.filename))
-            grayscale_img.save(grayscale_img_path)
-            return 
-
-# -----------------------------------------------------------------------------
 #  Execution
 # -----------------------------------------------------------------------------
+    
+    workfunc = functools.partial(realworker, moutput_path=output_path, mis_blur=is_blur, mis_grayscale=is_grayscale, mis_ste=is_ste, mste_percentage=ste_percentage, b_dir=blur_dir, s_dir=ste_dir, g_dir=grayscale_dir)
 
+    p = Pool()
+    
     # carry out the degradation
     # not wrapping it in try-catch to get better error messages
-    for file_path in traverse(input_path):
-        img = TransformedImage(Image.open(file_path), output_path)
-        print("----------------------------------------------------------------")
-        print("Image: %s" % img.img.filename)
-        if is_blur: print("Complete blur in %0.3f seconds" % timeit(img.blur))
-        if is_grayscale: print("Complete grayscale in %0.3f seconds" % timeit(img.grayscale))
-        if is_ste: print("Complete shrink then enlarge in %0.3f seconds" % timeit(img.shrink_then_enlarge, ste_percentage))
+    atOnce = 100
+    for file_paths in grouper(atOnce,traverse(input_path)):
+
+        retval = p.map(workfunc, file_paths)
+        for r in retval:
+            print(r)
+        #retval = realworker(file_paths[0],output_path, is_blur, is_grayscale, is_ste, ste_percentage)
+        #retval = workfunc(file_paths[0])
+      
+        #print(retval,type(retval))
+
     return
 
 # -----------------------------------------------------------------------------
 #  Helper functions
 # -----------------------------------------------------------------------------
-
-def timeit(func, *args, **kwargs):
-    """ This is a wrapper function to calculate how fast each operation takes.
-    Note that we are assuming that we do not need to do anything with the return
-    value of the function.
-    Args: 
-        func: function pointer
-        args: arguments to the function
-        kwargs: named arguments not defined in advance to be passed in to the function
-    """
-    start = time.time()
-    func(*args, **kwargs)
-    elapsed = time.time() - start
-    return elapsed
 
 def makepath(dirname, basename):
     """ Create a full path given the directory name and the base name
