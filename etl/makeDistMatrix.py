bbe7fdf0ef62720e4553a2e1ee434b878f0bd7f5
d grossman <dgrossman@iqt.org>
2016-12-09 16:49:09 +0000

    adding etl scripts for vector and matrix generation

---
+++ etl/makeDistMatrix.py
@@ -0,0 +1,85 @@
+import json
+import sys
+from multiprocessing import Pool
+import scipy.spatial.distance
+import itertools
+import numpy as np
+import time
+
+
+# read the list of things to compare
+def makeWork(vectorFileName):
+    vfile = open(vectorFileName, 'r')
+    retval = list()
+    for line in vfile:
+        line = line.strip()
+        line = json.loads(line)
+        retval.append(line)
+    vfile.close()
+    return retval
+
+
+# help by chopping work into chunks
+def grouper(n, iterable):
+    it = iter(iterable)
+    while True:
+        chunk = tuple(itertools.islice(it, n))
+        if not chunk:
+            return
+        yield chunk
+
+
+# my distance measures
+def my_dist(workList):
+    retval = list()
+
+    for pair in workList:
+        x = pair[0]
+        y = pair[1]
+        fx = np.asarray(x['resnet50'])
+        fy = np.asarray(y['resnet50'])
+        workItem = dict()
+        dc = str(float(scipy.spatial.distance.cosine(fx, fy)))
+        de = str(float(scipy.spatial.distance.euclidean(fx, fy)))
+        workItem['x'] = x['imageName']
+        workItem['y'] = y['imageName']
+        workItem['cosine'] = dc
+        workItem['euclidean'] = de
+        retval.append(workItem)
+
+    return (retval)
+
+
+def main(pworkers=15, atOnceOuter=100000, atOnceInner=10000):
+
+    inFileName = sys.argv[1]
+    work = makeWork(inFileName)
+    p = Pool(pworkers)
+
+    outFileName = 'matrixFile.{0}'.format(inFileName)
+    matrixFile = open(outFileName, 'w')
+
+    total = 0
+    for batch in grouper(atOnceOuter, itertools.combinations(work, 2)):
+        start = time.time()
+        batched = list()
+
+        for workbatch in grouper(atOnceInner, batch):
+            batched.append(workbatch)
+
+        retval = p.map(my_dist, batched)
+        end = time.time()
+        start2 = time.time()
+        for listLine in retval:
+            for line in listLine:
+                total = total + 1
+                matrixFile.write(json.dumps(line)+'\n')
+        end2 = time.time()
+
+    fstr = 'proc elapsed:{0} sec proc:{1} total{2}'
+    print(fstr.format(end-start, atOnceOuter, total))
+    print('IO elapsed:{0}\n'.format(end2-start2))
+    matrixFile.close()
+
+if __name__ == '__main__':
+    main()
---
+++ etl/makeFeaturesResNet50.py
@@ -0,0 +1,84 @@
+import numpy as np
+from resnet50 import ResNet50
+from keras.preprocessing import image
+from imagenet_utils import preprocess_input
+from keras.models import Model
+import os
+import time
+import json
+import sys
+
+
+# return an image from a file, default resize to 224,224
+def load_image(img_path,resizex=224, resizey=224):
+    data = image.load_img(img_path, target_size=(resizex, resizey))
+    x = image.img_to_array(data)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    return x
+
+
+# load the imagenet networks
+def get_models():
+    # include_top needs to be True for this to work
+    base_model = ResNet50(weights='imagenet', include_top=True)
+    model = Model(input=base_model.input,
+                  output=base_model.get_layer('flatten_1').output)
+    return (model, base_model)
+
+
+# return feature vector for a given img, and model
+def image_features(img, model):
+    features = model.predict(img)
+    return features
+
+
+# read the files to process
+def getList(name):
+    retval = list()
+    f = open('name', 'r')
+    for line in f:
+        line = line.strip()
+        line = json.loads(line)
+        retval.append(line)
+    f.close()
+    return retval
+
+
+# perform the file by file processing
+def process(trainingList, prefix, model, outFilename, batchSize=1000):
+    outFile = open(outFilename, 'w')
+    start = time.time()
+    for idx, line in enumerate(trainingList):
+        tempd = dict()
+        if idx % batchSize == 0:
+            end = time.time() - start
+            start = time.time()
+            fstring = 'total {0} batch {1} images in {2} seconds'
+            print (fstring.format(idx, batchSize, end))
+            path = os.path.join(prefix, line['imageName'])
+            img = load_image(path)
+            feature = image_features(img, model)
+            tempd['resnet50'] = feature.tolist()[0]
+            tempd.update(line)
+            outFile.write(json.dumps(tempd)+'\n')
+    outFile.close()
+
+
+def main():
+    prefix = sys.argv[1]
+    lineFilename = sys.argv[2]
+    outFilename = '{0}.resnet50.json'.format(sys.argv[2])
+    model, base_model = get_models()
+
+    print('loading...')
+    trainingList = getList(lineFilename)
+
+    print('processing...')
+    process(trainingList, prefix,  model, outFilename)
+
+    print('done.')
+
+
+if __name__ == '__main__':
+    main()
