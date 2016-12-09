0ead34b3263ccbe616169c4db26210fea2681d5b
d grossman <dgrossman@iqt.org>
2016-12-09 17:36:42 +0000

    file of filenames to file of json...

---
+++ etl/veriFileList2Json.py
@@ -0,0 +1,26 @@
+import json
+import sys
+
+# turn the list of files into json for working with
+def main():
+    inFileName = sys.argv[1]
+    outFileName = '{0}.json'.format(inFileName)
+
+    inFile = open(inFileName, 'r')
+    outFile = open(outFileName, 'w')
+
+    for line in inFile:
+        d = dict()
+        line = line.strip()
+        attrs = line.split('_')
+        d['imageName'] = line
+        d['vehicleID'] = attrs[0]
+        d['cameraID'] = attrs[1]
+        d['colorID'] = str(-1)
+        d['typeID'] = str(-1)
+        outFile.write(d+'\n')
+    inFile.close()
+
+
+if __name__ == '__main__':
+    main()
