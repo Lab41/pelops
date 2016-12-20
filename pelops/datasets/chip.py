da542fb1e28b9c25958ea4a707ba1c552bfbf686
Tiffany J <tiffanyj@iqt.org>
2016-12-20 11:10:13 -0800

    rework chip

--- pelops/datasets/chip.py
+++ pelops/datasets/chip.py
@@ -2,6 +2,7 @@
 """
 
 import abc
+import collections
 import os
 
 # ================================================================================
@@ -24,24 +25,20 @@ class DatasetFactory(object):
 class ChipDataset(metaclass = abc.ABCMeta):
     def __init__(self, dataset_path):
         self.dataset_path = dataset_path
-        self.chips = list()
+        self.chips = dict()
 
     @classmethod
     def check_dataset_type(self, dataset_type):
         return dataset_type == self.__name__
 
-    @abc.abstractmethod
-    def get_all_chips(self):
-        pass
-
     def get_all_chips_by_car_id(self, car_id):
-        return [chip for chip in self.chips if chip.car_id == car_id]
+        return [chip for chip in self.chips.values() if chip.car_id == car_id]
 
     def get_all_chips_by_camera_id(self, camera_id):
-        return [chip for chip in self.chips if chip.camera_id == camera_id]
+        return [chip for chip in self.chips.values() if chip.cam_id == cam_id]
 
     def __iter__(self):
-        for chip in self.chips:
+        for chip in self.chips.values():
             yield chip
         raise StopIteration()
 
@@ -53,26 +50,9 @@ class ChipDataset(metaclass = abc.ABCMeta):
 # ================================================================================
 
 
-class ChipBase(metaclass = abc.ABCMeta):
-    # TODO: handle misc arguments
-    def __init__(self, filepath, car_id, camera_id, timestamp):
-        self.name = os.path.basename(os.path.normpath(filepath))
-        self.filepath = filepath
-        self.car_id = car_id
-        self.camera_id = camera_id
-        self.timestamp = timestamp
-
-    @classmethod
-    def check_chip_type(self, chip_type):
-        return chip_type == self.__name__
-
-    def __hash__(self):
-        return hash(self.name)
-
-    def __eq__(self, other):
-        # check that dataset_type, car_id, camera_id, and timestamp are the same
-        return (type(self) == type(other)) and \
-               (self.car_id == other.car_id) and \
-               (self.camera_id == other.camera_id) and \
-               (self.timestamp == other.timestamp)
-
+class Chip = collections.namedtuple("Chip", 
+    ["filepath",
+     "car_id", 
+     "cam_id", 
+     "time",
+     "misc"])
