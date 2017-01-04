efb792faf6e9354c5a23c93015d963aaf08552e2
Yonas Tesfaye <ymt123@alum.mit.edu>
2017-01-04 12:01:54 -0800

    Speeding up chip base class operations

--- pelops/datasets/chip.py
+++ pelops/datasets/chip.py
@@ -25,21 +25,41 @@ class ChipDataset(metaclass = abc.ABCMeta):
         self.dataset_path = dataset_path
         self.set_type = set_type
         self.chips = dict()
+        self.chips_by_cam_id = None
+        self.chips_by_car_id = None
 
     @classmethod
     def check_dataset_type(self, dataset_type):
         return dataset_type == self.__name__
 
     def get_all_chips_by_car_id(self, car_id):
-        return [chip for chip in self.chips.values() if chip.car_id == car_id]
+        if self.chips_by_car_id is None:
+            self.chips_by_car_id = collections.defaultdict(list)
+            for chip_key, chip in self.chips.items():
+                self.chips_by_car_id[chip.car_id].append(chip_key)
+        return [self.chips[chip_key] for chip_key in self.chips_by_car_id[car_id]]        
+    
+    def get_all_chips_by_car_id_camera_id(self, car_id, cam_id):
+        output = []
+        for chip in self.get_all_chips_by_car_id(car_id):
+            if chip.cam_id == cam_id:
+                output.append(chip)
+        return output
 
     def get_all_chips_by_cam_id(self, cam_id):
-        return [chip for chip in self.chips.values() if chip.cam_id == cam_id]
+        if self.chips_by_cam_id is None:
+            self.chips_by_cam_id = collections.defaultdict(list)
+            for chip_key, chip in self.chips.items():
+                self.chips_by_cam_id[chip.cam_id].append(chip_key)
+                
+        return [self.chips[chip_key] for chip_key in self.chips_by_cam_id[cam_id]]
 
     def get_distinct_cams_by_car_id(self, car_id):
+        # TODO: Look at performance
         return self.get_distinct_cams_per_car()[car_id]
 
     def get_distinct_cams_per_car(self):
+        # TODO: Look at performance
         list_of_cameras_per_car = collections.defaultdict(set)
         for chip in self.chips.values():
             list_of_cameras_per_car[chip.car_id].add(chip.cam_id)
