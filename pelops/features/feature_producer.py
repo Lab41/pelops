f4ad622dbfb781a1879dfc12d3305ccb29c1d4d9
Yonas Tesfaye <ymt123@alum.mit.edu>
2017-03-09 13:13:01 -0800

    Add ability to save feature dataset

--- pelops/features/feature_producer.py
+++ pelops/features/feature_producer.py
@@ -2,6 +2,7 @@ import numpy as np
 from PIL import Image
 
 from pelops.datasets.chipper import Chipper
+from pelops.datasets.featuredataset import FeatureDataset
 
 
 class FeatureProducer(object):
@@ -12,15 +13,23 @@ class FeatureProducer(object):
     def return_features(self):
         if isinstance(self.chip_producer, Chipper):
             chips = []
+            chip_keys = []
             for chip_list in self.chip_producer:
                 chips.extend(chip_list)
+                for i, chip in enumerate(chip_list):
+                    chip_keys.append('{}_{}'.format(chip.frame_number, i))
+
         else:
-            chips = self.chip_producer.chips.values()
+            chips = []
+            chip_keys = []
+            for chip_key, chip in self.chip_producer.chips:
+                chips.append(chip)
+                chip_keys.append(chip_key)
 
         feats = np.zeros((len(chips), self.feat_size), dtype=np.float32)
         for i, chip in enumerate(chips):
             feats[i] = self.produce_features(chip)
-        return chips, feats
+        return chip_keys, chips, feats
 
     @staticmethod
     def get_image(chip):
@@ -34,6 +43,21 @@ class FeatureProducer(object):
         """Takes a chip object and returns a feature vector of size
         self.feat_size. """
         raise NotImplementedError("produce_features() not implemented")
+
+    def save_features(self, output_filename):
+        """
+        Calculate features and save as a "FeatureDataset"
+        Args:
+            filename:
+
+        Returns:
+
+        """
+        # TODO: See if this function should save the features in memory
+        if isinstance(self.chip_producer, Chipper):
+            raise NotImplementedError("Only ChipDatasets are supported at this time")
+        chip_keys, chips, features = self.return_features()
+        FeatureDataset.save(output_filename, chip_keys, chips, features)
 
     def set_variables(self):
         """Child classes should use this to set self.feat_size, and any other
