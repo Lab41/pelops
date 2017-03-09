c3fa110a9762b444382681f41f1b8cb1219d26f9
Alexander Gude <agude@iqt.org>
2017-03-09 21:37:19 +0000

    Change tests to compare image data

--- testci/test_featureproducer.py
+++ testci/test_featureproducer.py
@@ -4,14 +4,11 @@ import pytest
 import numpy as np
 from PIL import Image
 
-#from pelops.datasets.chip import ChipDataset, Chip
-from pelops.datasets.featuredataset import FeatureDataset
 from pelops.features.feature_producer import FeatureProducer
 
 
 @pytest.fixture
-def chip_producer():
-    Chip = collections.namedtuple("Chip", ["filepath", "car_id", "cam_id", "time", "img_data", "misc"])
+def img_data():
     DATA = [[[  0,   0,   0],
              [255, 255, 255],
              [  0,   0,   0]],
@@ -21,9 +18,15 @@ def chip_producer():
             [[  0,   0,   0],
              [255, 255, 255],
              [  0,   0,   0]]]
+    return np.array(DATA, dtype=np.uint8)
+
+
+@pytest.fixture
+def chip_producer(img_data):
+    Chip = collections.namedtuple("Chip", ["filepath", "car_id", "cam_id", "time", "img_data", "misc"])
     CHIPS = (
         # filepath, car_id, cam_id, time, img_data, misc
-        ("car1_cam1.png", 1, 1, datetime.datetime(2016, 10, 1, 0, 1, 2, microsecond=100), np.array(DATA, dtype=np.uint8), {}),
+        ("car1_cam1.png", 1, 1, datetime.datetime(2016, 10, 1, 0, 1, 2, microsecond=100), img_data, {}),
     )
 
     chip_producer = {"chips": {}}
@@ -34,6 +37,7 @@ def chip_producer():
 
     return chip_producer
 
+
 @pytest.fixture
 def monkey_feature_producer(chip_producer):
     # Monkey patch the __init__() function so that it will succeed
@@ -42,7 +46,7 @@ def monkey_feature_producer(chip_producer):
 
     FeatureProducer.__init__ = new_init
 
-    return (FeatureProducer(chip_producer))
+    return FeatureProducer(chip_producer)
 
 
 def test_set_variables_raises():
@@ -55,7 +59,8 @@ def test_produce_features_raises(monkey_feature_producer):
         monkey_feature_producer.produce_features(None)
 
 
-def test_get_image_img_data(monkey_feature_producer, chip_producer):
+def test_get_image_img_data(monkey_feature_producer, chip_producer, img_data):
     for key, chip in chip_producer["chips"].items():
-        assert monkey_feature_producer.get_image(chip)
-
+        image = monkey_feature_producer.get_image(chip)
+        image_array = np.array(image)
+        assert np.array_equal(img_data, np.array(image))
