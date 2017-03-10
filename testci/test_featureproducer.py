0b46a52b67d15b7042795b5c8a7d3fac0375016e
Alexander Gude <agude@iqt.org>
2017-03-10 23:45:10 +0000

    Add tests that would have found the previous bug

--- testci/test_featureproducer.py
+++ testci/test_featureproducer.py
@@ -24,16 +24,17 @@ def img_data():
 @pytest.fixture
 def chip_producer(img_data):
     Chip = collections.namedtuple("Chip", ["filepath", "car_id", "cam_id", "time", "img_data", "misc"])
+    ChipProducer = collections.namedtuple("ChipProducer", ["chips"])
     CHIPS = (
         # filepath, car_id, cam_id, time, img_data, misc
         ("car1_cam1.png", 1, 1, datetime.datetime(2016, 10, 1, 0, 1, 2, microsecond=100), img_data, {}),
     )
 
-    chip_producer = {"chips": {}}
+    chip_producer = ChipProducer({})
     for filepath, car_id, cam_id, time, img_data, misc in CHIPS:
         print(img_data.shape)
         chip = Chip(filepath, car_id, cam_id, time, img_data, misc)
-        chip_producer["chips"][filepath] = chip
+        chip_producer.chips[filepath] = chip
 
     return chip_producer
 
@@ -43,6 +44,7 @@ def monkey_feature_producer(chip_producer):
     # Monkey patch the __init__() function so that it will succeed
     def new_init(self, chip_producer):
         self.chip_producer = chip_producer
+        self.feat_size = 1
 
     FeatureProducer.__init__ = new_init
 
@@ -60,7 +62,12 @@ def test_produce_features_raises(monkey_feature_producer):
 
 
 def test_get_image_img_data(monkey_feature_producer, chip_producer, img_data):
-    for key, chip in chip_producer["chips"].items():
+    for key, chip in chip_producer.chips.items():
         image = monkey_feature_producer.get_image(chip)
         image_array = np.array(image)
         assert np.array_equal(img_data, np.array(image))
+
+
+def test_return_features_raises(monkey_feature_producer):
+    with pytest.raises(NotImplementedError):
+        monkey_feature_producer.return_features()
