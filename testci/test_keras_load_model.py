30e0299bdb4dcf86ffb5e15bcbe9ba5f8fd9f65c
dgrossman <dgrossman@iqt.org>
2017-03-14 22:59:55 +0000

    circle ci path change

--- testci/test_keras_load_model.py
+++ testci/test_keras_load_model.py
@@ -4,8 +4,10 @@ from pelops.features.keras_model import KerasModelFeatureProducer
 
 
 def test_load_model_workaround():
-    model_filename = './small.json'
-    weight_filename = './small.hdf5'
+    # @TODO get some environment variable set when in CI environment
+    # test to see, modify path...
+    model_filename = '/home/ubuntu/pelops/testci/small.json'
+    weight_filename = '/home/ubuntu/pelops/testci/small.hdf5'
     model = KerasModelFeatureProducer.load_model_workaround(
         model_filename, weight_filename)
     assert model.layers[0].name == 'dense_1'
