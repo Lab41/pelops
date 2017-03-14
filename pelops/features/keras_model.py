0e46ae1b6b991c59b93cf3846dce7dd2caa99f34
dgrossman <dgrossman@iqt.org>
2017-03-14 20:09:42 +0000

    adding in support to use the load model workaround

--- pelops/features/keras_model.py
+++ pelops/features/keras_model.py
@@ -3,18 +3,37 @@ from keras.preprocessing import image
 from keras.applications.resnet50 import preprocess_input
 from keras.models import load_model
 from keras.models import Model
+from keras.models import model_from_json
 
 from PIL import Image as PIL_Image
 from pelops.features.feature_producer import FeatureProducer
 
 
 class KerasModelFeatureProducer(FeatureProducer):
-    def __init__(self, chip_producer, model_filename, layer_name):
+    def __init__(self, chip_producer, model_filename, layer_name, weight_filename=None):
         global resnet_model
         super().__init__(chip_producer)
-        self.original_model = load_model(model_filename)
+
+        if weight_filename is None:
+            self.original_model = load_model(model_filename)
+        else:
+            self.original_modle = load_model_workaround(model_filename,weight_filename)
+
         self.keras_model = Model(input=self.original_model.input,
                                  output=self.original_model.get_layer(layer_name).output)
+
+    @staticmethod
+    def load_model_workaround(model_filename,weight_filename):
+        # load json and create model
+        json_file = open(model_filename, 'r')
+        loaded_model_json = json_file.read()
+        json_file.close()
+
+        loaded_model = model_from_json(loaded_model_json)
+
+        # load weights into new model
+        loaded_model.load_weights(weight_filename)
+        return loaded_model
 
     @staticmethod
     def preprocess_image(img, x_dim=224, y_dim=224):
