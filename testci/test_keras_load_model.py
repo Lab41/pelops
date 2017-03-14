4501e28ebd0de9dd821b034a1264267956f0912c
dgrossman <dgrossman@iqt.org>
2017-03-14 22:45:12 +0000

    wrongname->correctname

--- testci/test_keras_load_model.py
+++ testci/test_keras_load_model.py
@@ -2,8 +2,10 @@ import pytest
 
 from pelops.features.keras_model import KerasModelFeatureProducer
 
+
 def test_load_model_workaround():
     model_filename = './small.json'
-    model_weights = './small.hdf5'
-    model = KerasModelFeatureProducer.load_model_workaround(model_filename,weight_filename)
+    weight_filename = './small.hdf5'
+    model = KerasModelFeatureProducer.load_model_workaround(
+        model_filename, weight_filename)
     assert model.layers[0].name == 'dense_1'
