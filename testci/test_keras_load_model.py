0eb46d9e79435b6e01f416031da7ba2ab52ccbf8
dgrossman <dgrossman@iqt.org>
2017-03-20 23:41:16 +0000

    should test better

--- testci/test_keras_load_model.py
+++ testci/test_keras_load_model.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 
 from pelops.features.keras_model import KerasModelFeatureProducer
@@ -6,8 +8,15 @@ from pelops.features.keras_model import KerasModelFeatureProducer
 def test_load_model_workaround():
     # @TODO get some environment variable set when in CI environment
     # test to see, modify path...
-    model_filename = '/home/ubuntu/pelops/testci/small.json'
-    weight_filename = '/home/ubuntu/pelops/testci/small.hdf5'
+    if os.getenv('CIRCLECI', 0) == 1:
+
+        model_filename = '/home/ubuntu/pelops/testci/small.json'
+        weight_filename = '/home/ubuntu/pelops/testci/small.hdf5'
+    else:
+        pwd = os.getcwd()
+        model_filename = os.path.join(pwd, 'testci/small.json')
+        weight_filename = os.path.join(pwd, 'testci/small.hdf5')
+
     model = KerasModelFeatureProducer.load_model_workaround(
         model_filename, weight_filename)
     assert model.layers[0].name == 'dense_8'
