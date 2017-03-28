98569cd3c7d59b06bf0ec7ded543f1658afe2f72
Alexander Gude <agude@iqt.org>
2017-03-28 20:18:15 +0000

    Update HOG tests to support multiple test images
    We now have three images that are tested:
    
    - An RGB image
    - An RGBA image
    - A grey scale image

--- testci/test_hog_feature.py
+++ testci/test_hog_feature.py
@@ -9,59 +9,116 @@ from itertools import product
 
 from pelops.features.hog import HOGFeatureProducer
 
-
-@pytest.fixture
-def img_data():
-    DATA = [[[  0,   0,   0],
-             [255, 255, 255],
-             [  0,   0,   0]],
-            [[255, 255, 255],
-             [  0,   0,   0],
-             [255, 255, 255]],
-            [[  0,   0,   0],
-             [255, 255, 255],
-             [  0,   0,   0]]]
-    return np.array(DATA, dtype=np.uint8)
-
-
-@pytest.fixture
-def pil_image(img_data):
-    img = Image.fromarray(img_data)
-    img = img.convert("RGB")
-    img = img.resize((256, 256), Image.BICUBIC)
-    return img
-
-
-@pytest.fixture
-def hog_features(pil_image):
-    img = color.rgb2gray(np.array(pil_image))
+def hog_features(img):
+    img = color.rgb2gray(np.array(img))
     features = hog(img, orientations=8, pixels_per_cell=(16, 16), cells_per_block=(16, 16))
-
     return features
 
 
-@pytest.fixture
-def color_features(pil_image):
-    hists = []
-    for channel in pil_image.split():
-        channel_array = np.array(channel)
-        values, _ = np.histogram(channel_array.flat, bins=256)
-        hists.append(values)
+def hist_features(img):
+        MAX_CHANNELS = 3
+        BINS = 256
 
-    return np.concatenate(hists)
+        channels = img.split()
+
+        # Remove alpha channels
+        if len(channels) > MAX_CHANNELS:
+            channels = channel[:MAX_CHANNELS]
+
+        # Calculate features
+        hist_features = np.zeros(MAX_CHANNELS * BINS)
+        for i, channel in enumerate(channels):
+            channel_array = np.array(channel)
+            values, _ = np.histogram(channel_array.flat, bins=BINS)
+            start = i * BINS
+            end = (i+1) * BINS
+            hist_features[start:end] = values
+
+        return hist_features
+
+
+@pytest.fixture(scope="module")
+def img_data():
+    data = {
+        "DATA_1":{},
+        "DATA_3":{},
+        "DATA_4":{},
+    }
+
+    # Raw data
+    data["DATA_1"]["array"] = np.array([
+        [[  0,   0,   0],
+         [255, 255, 255],
+         [  0,   0,   0]],
+    ], dtype=np.uint8)
+
+    data["DATA_3"]["array"] = np.array([
+        [[  0,   0,   0],
+         [255, 255, 255],
+         [  0,   0,   0]],
+        [[255, 255, 255],
+         [  0,   0,   0],
+         [255, 255, 255]],
+        [[  0,   0,   0],
+         [255, 255, 255],
+         [  0,   0,   0]],
+    ], dtype=np.uint8)
+
+    data["DATA_4"]["array"] = np.array([
+        [[  0,   0,   0],
+         [255, 255, 255],
+         [  0,   0,   0]],
+        [[255, 255, 255],
+         [  0,   0,   0],
+         [255, 255, 255]],
+        [[  0,   0,   0],
+         [255, 255, 255],
+         [  0,   0,   0]],
+        [[  0,   0,   0],
+         [  0,   0,   0],
+         [  0,   0,   0]],
+    ], dtype=np.uint8)
+
+    # PIL images
+    for data_id in data:
+        arr = data[data_id]["array"]
+        img = Image.fromarray(arr)
+        img = img.convert("RGB")
+        img = img.resize((256, 256), Image.BICUBIC)
+        data[data_id]["image"] = img
+
+    # Calculate HOG features
+    for data_id in data:
+        img = data[data_id]["image"]
+        hog = hog_features(img)
+        data[data_id]["hog_features"] = hog
+
+    # Calculate Histogram features
+    for data_id in data:
+        img = data[data_id]["image"]
+        hist = hist_features(img)
+        data[data_id]["hist_features"] = hist
+
+    return data
 
 
 @pytest.fixture
 def chip_producer(img_data):
     Chip = collections.namedtuple("Chip", ["filepath", "car_id", "cam_id", "time", "img_data", "misc"])
-    CHIPS = (
+    CHIPS = []
+    for i, data_id in enumerate(img_data):
+        data = img_data[data_id]
+        arr = data["array"]
+        # We use the data_id as the filepath since we do not actually open the
+        # file and it only needs to be unique
+        #
         # filepath, car_id, cam_id, time, img_data, misc
-        ("car1_cam1.png", 1, 1, datetime.datetime(2016, 10, 1, 0, 1, 2, microsecond=100), img_data, {}),
-    )
+        chip = (data_id, i, 1, datetime.datetime(2016, 10, 1, 0, 1, 2, microsecond=100+i), arr, {})
+        CHIPS.append(chip)
 
     chip_producer = {"chips": {}}
-    for filepath, car_id, cam_id, time, img_data, misc in CHIPS:
-        chip = Chip(filepath, car_id, cam_id, time, img_data, misc)
+    for filepath, car_id, cam_id, time, data, misc in CHIPS:
+        chip = Chip(filepath, car_id, cam_id, time, data, misc)
         chip_producer["chips"][filepath] = chip
 
     return chip_producer
@@ -74,16 +131,21 @@ def feature_producer(chip_producer):
     return hog
 
 
-def test_features(feature_producer, chip_producer, hog_features, color_features):
+def test_features(feature_producer, chip_producer, img_data):
     fp = feature_producer
-    hog_len = len(hog_features)
-    hist_len = len(color_features)
 
     for _, chip in chip_producer["chips"].items():
+        data_id = chip.filepath
+        data = img_data[data_id]
+        hog_features = data["hog_features"]
+        hist_features = data["hist_features"]
+        hog_len = len(hog_features)
+        hist_len = len(hist_features)
+
         features = feature_producer.produce_features(chip)
         assert len(features) == hog_len + hist_len
 
-        total_features = np.concatenate((hog_features, color_features))
+        total_features = np.concatenate((hog_features, hist_features))
         assert np.array_equal(features, total_features)
 
 
