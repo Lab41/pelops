15eb35f2017931d11edcfa6e776e0a2df42f8e30
Alexander Gude <agude@users.noreply.github.com>
2017-03-29 16:10:16 -0700

    Add color histograms to HOG extractor (#82)
    * Add color histograms to HOG extractor
    
    This starts to address #59.
    
    * Add warning about non-RGB images in HOG extractor
    
    * Update HOG tests to support multiple test images
    
    We now have three images that are tested:
    
    - An RGB image
    - An RGBA image
    - A grey scale image
    
    * Fix initial array value to -1 instead of 0
    
    This is what the documentation says, so we should probably do it that
    way.
    
    * Change image input size for HOG to 224x224
    
    This matches ResNet, which means that our image rescaling code will do
    the same thing for both HOG and ResNet, giving us something more
    directly comparable.

--- pelops/features/hog.py
+++ pelops/features/hog.py
@@ -8,10 +8,11 @@ from pelops.features.feature_producer import FeatureProducer
 
 class HOGFeatureProducer(FeatureProducer):
 
-    def __init__(self, chip_producer, image_size=(256,256), cells=(16, 16), orientations=8):
+    def __init__(self, chip_producer, image_size=(224,224), cells=(16, 16), orientations=8, histogram_bins_per_channel=256):
         self.image_size = image_size
         self.cells = cells
         self.orientations = orientations
+        self.histogram_bins_per_channel = histogram_bins_per_channel
         super().__init__(chip_producer)
 
     def produce_features(self, chip):
@@ -20,6 +21,27 @@ class HOGFeatureProducer(FeatureProducer):
         img = self.get_image(chip)
         img = img.resize(self.image_size, Image.BICUBIC)
         img_x, img_y = img.size
+
+        # Calculate histogram of each channel
+        channels = img.split()
+        hist_features = np.full(shape=3 * self.histogram_bins_per_channel, fill_value=-1)
+
+        # We expect RGB images. If something else is passed warn the user and
+        # continue.
+        if len(channels) < 3:
+            print("Non-RBG image! Vector will be padded with -1!")
+        if len(channels) > 3:
+            print("Non-RBG image! Channels beyond the first three will be ignored!")
+            channels = channel[:3]
+
+        for i, channel in enumerate(channels):
+            channel_array = np.array(channel)
+            values, _ = np.histogram(channel_array.flat, bins=self.histogram_bins_per_channel)
+            start = i * self.histogram_bins_per_channel
+            end = (i+1) * self.histogram_bins_per_channel
+            hist_features[start:end] = values
+
+        # Calculate HOG features, which require a grayscale image
         img = color.rgb2gray(np.array(img))
         features = hog(
             img,
@@ -27,7 +49,10 @@ class HOGFeatureProducer(FeatureProducer):
             pixels_per_cell=(img_x / self.cells[0], img_y / self.cells[1]),
             cells_per_block=self.cells,  # Normalize over the whole image
         )
-        return features
+
+        return np.concatenate((features, hist_features))
 
     def set_variables(self):
-        self.feat_size = self.cells[0] * self.cells[1] * self.orientations
+        hog_size = self.cells[0] * self.cells[1] * self.orientations
+        hist_size = 3 * self.histogram_bins_per_channel
+        self.feat_size = hog_size + hist_size
--- testci/test_hog_feature.py
+++ testci/test_hog_feature.py
@@ -9,43 +9,116 @@ from itertools import product
 
 from pelops.features.hog import HOGFeatureProducer
 
-
-@pytest.fixture
-def img_data():
-    DATA = [[[  0,   0,   0],
-             [255, 255, 255],
-             [  0,   0,   0]],
-            [[255, 255, 255],
-             [  0,   0,   0],
-             [255, 255, 255]],
-            [[  0,   0,   0],
-             [255, 255, 255],
-             [  0,   0,   0]]]
-    return np.array(DATA, dtype=np.uint8)
-
-
-@pytest.fixture
-def hog_features(img_data):
-    img = Image.fromarray(img_data)
-    img = img.convert("RGB")
-    img = img.resize((256, 256), Image.BICUBIC)
+def hog_features(img):
     img = color.rgb2gray(np.array(img))
-    features = hog(img, orientations=8, pixels_per_cell=(16, 16), cells_per_block=(16, 16))
-
+    features = hog(img, orientations=8, pixels_per_cell=(14, 14), cells_per_block=(16, 16))
     return features
+
+
+def hist_features(img):
+        MAX_CHANNELS = 3
+        BINS = 256
+
+        channels = img.split()
+
+        # Remove alpha channels
+        if len(channels) > MAX_CHANNELS:
+            channels = channel[:MAX_CHANNELS]
+
+        # Calculate features
+        hist_features = np.zeros(MAX_CHANNELS * BINS)
+        for i, channel in enumerate(channels):
+            channel_array = np.array(channel)
+            values, _ = np.histogram(channel_array.flat, bins=BINS)
+            start = i * BINS
+            end = (i+1) * BINS
+            hist_features[start:end] = values
+
+        return hist_features
+
+
+@pytest.fixture(scope="module")
+def img_data():
+    data = {
+        "DATA_1":{},
+        "DATA_3":{},
+        "DATA_4":{},
+    }
+
+    # Raw data
+    data["DATA_1"]["array"] = np.array([
+        [[  0,   0,   0],
+         [255, 255, 255],
+         [  0,   0,   0]],
+    ], dtype=np.uint8)
+
+    data["DATA_3"]["array"] = np.array([
+        [[  0,   0,   0],
+         [255, 255, 255],
+         [  0,   0,   0]],
+        [[255, 255, 255],
+         [  0,   0,   0],
+         [255, 255, 255]],
+        [[  0,   0,   0],
+         [255, 255, 255],
+         [  0,   0,   0]],
+    ], dtype=np.uint8)
+
+    data["DATA_4"]["array"] = np.array([
+        [[  0,   0,   0],
+         [255, 255, 255],
+         [  0,   0,   0]],
+        [[255, 255, 255],
+         [  0,   0,   0],
+         [255, 255, 255]],
+        [[  0,   0,   0],
+         [255, 255, 255],
+         [  0,   0,   0]],
+        [[  0,   0,   0],
+         [  0,   0,   0],
+         [  0,   0,   0]],
+    ], dtype=np.uint8)
+
+    # PIL images
+    for data_id in data:
+        arr = data[data_id]["array"]
+        img = Image.fromarray(arr)
+        img = img.convert("RGB")
+        img = img.resize((224, 224), Image.BICUBIC)
+        data[data_id]["image"] = img
+
+    # Calculate HOG features
+    for data_id in data:
+        img = data[data_id]["image"]
+        hog = hog_features(img)
+        data[data_id]["hog_features"] = hog
+
+    # Calculate Histogram features
+    for data_id in data:
+        img = data[data_id]["image"]
+        hist = hist_features(img)
+        data[data_id]["hist_features"] = hist
+
+    return data
 
 
 @pytest.fixture
 def chip_producer(img_data):
     Chip = collections.namedtuple("Chip", ["filepath", "car_id", "cam_id", "time", "img_data", "misc"])
-    CHIPS = (
+    CHIPS = []
+    for i, data_id in enumerate(img_data):
+        data = img_data[data_id]
+        arr = data["array"]
+        # We use the data_id as the filepath since we do not actually open the
+        # file and it only needs to be unique
+        #
         # filepath, car_id, cam_id, time, img_data, misc
-        ("car1_cam1.png", 1, 1, datetime.datetime(2016, 10, 1, 0, 1, 2, microsecond=100), img_data, {}),
-    )
+        chip = (data_id, i, 1, datetime.datetime(2016, 10, 1, 0, 1, 2, microsecond=100+i), arr, {})
+        CHIPS.append(chip)
 
     chip_producer = {"chips": {}}
-    for filepath, car_id, cam_id, time, img_data, misc in CHIPS:
-        chip = Chip(filepath, car_id, cam_id, time, img_data, misc)
+    for filepath, car_id, cam_id, time, data, misc in CHIPS:
+        chip = Chip(filepath, car_id, cam_id, time, data, misc)
         chip_producer["chips"][filepath] = chip
 
     return chip_producer
@@ -58,25 +131,37 @@ def feature_producer(chip_producer):
     return hog
 
 
-def test_features(feature_producer, chip_producer, hog_features):
+def test_features(feature_producer, chip_producer, img_data):
     fp = feature_producer
+
     for _, chip in chip_producer["chips"].items():
+        data_id = chip.filepath
+        data = img_data[data_id]
+        hog_features = data["hog_features"]
+        hist_features = data["hist_features"]
+        hog_len = len(hog_features)
+        hist_len = len(hist_features)
+
         features = feature_producer.produce_features(chip)
-        assert len(features) == fp.cells[0] * fp.cells[1] * fp.orientations
-        assert np.array_equal(features, hog_features)
+        assert len(features) == hog_len + hist_len
+
+        total_features = np.concatenate((hog_features, hist_features))
+        assert np.array_equal(features, total_features)
 
 
 def test_inputs(chip_producer):
-    pix_sizes = (32, 64, 126, 256, 512)
+    pix_sizes = (32, 64, 128, 256, 512)
     cell_counts = (1, 2, 4, 16)
     orientation_counts = (2, 4, 8, 16)
-    for pix, cell, orientation in product(pix_sizes, cell_counts, orientation_counts):
+    histogram_bins = (32, 64, 128, 256)
+    for pix, cell, orientation, histogram_bin in product(pix_sizes, cell_counts, orientation_counts, histogram_bins):
         hog = HOGFeatureProducer(
             chip_producer,
             image_size=(pix, pix),
             cells=(cell, cell),
             orientations=orientation,
+            histogram_bins_per_channel=histogram_bin,
         )
         for _, chip in chip_producer["chips"].items():
             features = hog.produce_features(chip)
-            assert len(features) == (cell**2) * orientation
+            assert len(features) == ((cell**2) * orientation) + (3 * histogram_bin)
